a7612014999026e8d99fb59faf654a652f6e2c27
Frédéric VIRICEL <99799464+xxxPOUPOUxxx@users.noreply.github.com>
2025-10-19 22:25:15 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/cryptobase/main.go
@@ -0,0 +1,23 @@
+// cmd/cryptobase/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "cryptobase/internal/cryptobase"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := cryptobase.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
